19f8779e3aea42a834cbdc055765420fc91416ff
sekhar <sekhar@nbostech.com>
2016-02-15 21:09:47 +0530

    Small fix for instagram login.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
-  provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
+  provider :instagram, ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET']
 end
 
 
